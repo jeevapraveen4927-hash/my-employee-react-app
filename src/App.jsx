c75e7fe3a5f70c4cc36f018d024feddf96ca8b4b
jeevapraveen4927-hash <jeevapraveen4927@gmail.com>
2026-06-15 13:54:48 +0530

    add Student Component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import Student from './components/Student'
 
 
 function App() {
-  return(
-    <ListEmployeeComponent/>
+  return (
+    <div>
+      <ListEmployeeComponent />
+      
+
+      <Student
+        name="John Doe"
+        age={20}
+        course="Computer Science"
+      />
+
+      <Student
+        name="Jane Smith"
+        age={21}
+        course="Information Technology"
+      />
+    </div>
   );
 }
-export default App;
-  
 
-  
+export default App
